f28b257cd3c2d96a69dd85da098f21e8bfcfccfb
Robert Csakany <csakany.robert@gmail.com>
2017-01-28 00:27:11 +0100

    v1.0.1 Fixing parameters

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export POM_UPDATE_PARENT_FILE=${POM_UPDATE_PARENT_FILE:-"pom.xml"}
+export WERCKER_POM_UPDATE_PARENT_POM=${WERCKER_POM_UPDATE_PARENT_POM:-"pom.xml"}
 python -c "import xml.etree.ElementTree as ET; \
 	ET.register_namespace('','http://maven.apache.org/POM/4.0.0'); \
-	tree=ET.parse(open('$POM_UPDATE_PARENT_VERSION')); \
-	tree.getroot().find('{http://maven.apache.org/POM/4.0.0}parent').find('{http://maven.apache.org/POM/4.0.0}version').text='$POM_UPDATE_PARENT'; \
-	tree.write('$POM_UPDATE_PARENT_FILE/pom.xml')"
+	tree=ET.parse(open('$WERCKER_POM_UPDATE_PARENT_POM')); \
+	tree.getroot().find('{http://maven.apache.org/POM/4.0.0}parent').find('{http://maven.apache.org/POM/4.0.0}version').text='$WERCKER_POM_UPDATE_PARENT_VERSION'; \
+	tree.write('$WERCKER_POM_UPDATE_PARENT_POM')"
 
